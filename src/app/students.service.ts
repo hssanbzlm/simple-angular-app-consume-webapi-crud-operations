1c970430bde90fb6512e850ae56656f235e06ae8
HssanBouzlima <bouzlima.h@live.fr>
2019-08-18 02:11:30 +0100

    Update students.service.ts

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -13,19 +13,19 @@ export class StudentsService {
 
    GetAllStudent():Observable<Student[]>
    {
-    var  url="http://localhost:65186/api/students";
+    var  url="http://localhost:65186/api/students";//asp.net web api
     return this.http.get<Student[]>(url);
    } 
 
    onDelete(Id:number)
    {  
-       var url="http://localhost:65186/api/students/"+Id;
+       var url="http://localhost:65186/api/students/"+Id;//asp.net web api
        return this.http.delete(url);
 
    }
    onUpdate(Id:number,s:Student):Observable<any>
    {    
-     var url="http://localhost:65186/api/students/"+Id;
+     var url="http://localhost:65186/api/students/"+Id;//asp.net web api
 
      return this.http.put(url,s);
       
@@ -35,14 +35,14 @@ export class StudentsService {
    GetStudent(Id:number):Observable<Student>
    { 
 
-    var url="http://localhost:65186/api/students/"+Id;
+    var url="http://localhost:65186/api/students/"+Id;//asp.net web api
     return this.http.get<Student>(url);
 
    } 
 
    AddStudent(s:Student):Observable<any>
    { 
-     var url="http://localhost:65186/api/students"
+     var url="http://localhost:65186/api/students"//asp.net web api
     return this.http.post(url,s);
    }
     
